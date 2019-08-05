5b3d314a0053f79d9e546cd50766b52b05ae806a
nicole gerbauld <nicole.gerbauld@gmail.com>
2019-08-06 00:11:19 +0800

    added form to create project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,8 +9,13 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    @project.save
-    redirect_to root_path
+    @project.user = current_user
+    authorize @project
+    if @project.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def update
