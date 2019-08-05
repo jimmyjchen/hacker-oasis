f891bead3d4f16b26cae99bcc33871b773333a57
BingQ <49521135+BingQzzz@users.noreply.github.com>
2019-08-05 02:21:58 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :projects
   has_many :teams
-  has_many :projects
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
