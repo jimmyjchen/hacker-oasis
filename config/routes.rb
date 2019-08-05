9550b5b2a03ed0f86ce3d3cb8a3b3d8ac69ebe0f
nicole gerbauld <nicole.gerbauld@gmail.com>
2019-08-06 00:23:31 +0800

    create project btn added in show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
 
   root to: 'projects#index'
 
-  # root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
