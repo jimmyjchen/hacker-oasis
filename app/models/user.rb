b53b24178546de89889c5f372e94351dd55b8d89
root <ERDeWees@gmail.com>
2019-08-06 21:05:50 +0800

    avatar added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- has_many :projects
- has_many :collaborations
+  mount_uploader :avatar, PhotoUploader
+  has_many :projects
+  has_many :collaborations
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
