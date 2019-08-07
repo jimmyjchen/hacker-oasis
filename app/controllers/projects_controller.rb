27761123a312c28062663969d9049d6bb3baf925
BingQ <49521135+BingQzzz@users.noreply.github.com>
2019-08-07 00:31:33 -0700

    Update projects_controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -41,7 +41,7 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:name, :description, :cover_photo, :photo1, :photo2, :photo3,)
+    params.require(:project).permit(:name, :description, :cover_photo, :photo1, :photo2, :photo3, :photo_cache)
   end
 
   def set_project
