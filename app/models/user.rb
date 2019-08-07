d1a058a96778392f24529089194c6412f8ece1d7
Jimmy Chen <jimmyjchen@outlook.com>
2019-08-07 15:51:39 +0800

    Avatar and photos working now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  mount_uploader :avatar, PhotoUploader
+  mount_uploader :avatar, AvatarUploader
   has_many :projects
   has_many :collaborations
   has_many :comments
