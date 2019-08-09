7d9dddd1608da21c24f658cd502e36c4d66e0ddf
Bingqing Zhu <bingbing13@163.com>
2019-08-08 23:38:32 -0700

    added github oauth and chose avatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
 
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  # devise_scope :user do
+  #   get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+
   resources :projects do
     resources :collaborations, only: [:new, :create, :show, :index]
     resources :comments, only: [:new, :create]
