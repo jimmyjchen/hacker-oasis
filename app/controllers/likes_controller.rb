17c57d74da43c10351f362c0847b3f426d6e9e9f
nicole gerbauld <nicole.gerbauld@gmail.com>
2019-08-13 15:59:50 +0800

    changed the likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,23 @@ class LikesController < ApplicationController
   before_action :find_project
 
   def create
-    @project.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      @project.likes.create(user_id: current_user.id)
+    end
     redirect_to project_path(@project)
   end
 
   private
 
+  def already_liked?
+    Like.where(user_id: current_user.id, project_id:
+    params[:project_id]).exists?
+  end
+
   def find_project
     @project = Project.find(params[:project_id])
   end
+
 end
