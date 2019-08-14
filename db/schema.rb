5cf5a1b30b8827cc4f3f8a15ac41645847719de5
nicole gerbauld <nicole.gerbauld@gmail.com>
2019-08-14 12:56:12 +0800

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_070220) do
+ActiveRecord::Schema.define(version: 2019_08_14_043051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,11 +81,11 @@ ActiveRecord::Schema.define(version: 2019_08_13_070220) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.string "url"
     t.string "qrcode"
     t.string "type"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
@@ -139,12 +139,13 @@ ActiveRecord::Schema.define(version: 2019_08_13_070220) do
     t.string "description"
     t.string "avatar"
     t.string "username"
-    t.string "provider"
-    t.string "uid"
     t.string "wechatid"
     t.string "linkedin"
     t.string "github"
+    t.string "provider"
+    t.string "uid"
     t.string "social_avatar"
+    t.text "skills", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
