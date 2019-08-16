03c491c44f31dd62969049dfcd511f94639c3c7a
Jimmy Chen <jimmyjchen@outlook.com>
2019-08-16 15:09:00 +0800

    debug map zoom

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -23,7 +23,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds);
+  map.fitBounds(bounds, {padding: 70, maxZoom: 15});
 };
 
 const initMapbox = () => {
